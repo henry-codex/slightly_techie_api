403a4a18c4058e17fcc00dfeb8a1b47a06b1b1b7
henry-codex <httetteh@st.ug.edu.gh>
2024-10-31 11:39:47 +0000

    initiated app

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get(' ', (req, res) => {
+app.get('', (req, res) => {
     res.send('Hello World');
     });
 
